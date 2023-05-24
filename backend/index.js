03c4e2a7000009637335629cd2a94504d90cd409
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-05-23 20:30:06 -0700

    downgraded mongoose

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,11 @@ const dotenv = require("dotenv")
 
 dotenv.config()
 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log("connected to mongo") }
+  )
+
+
 app.listen(3000, () => {
     console.log("running")
 })
