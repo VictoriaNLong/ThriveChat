1055cb88800b986b9d77a1ea4782608da5293e75
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-05-25 18:55:47 -0700

    added login and user routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,53 @@
+const User = require("../models/User")
 const router = require("express").Router()
+const bcrypt = require("bcrypt")
 
-router.get("/",(req,res) => {
-    res.send("user route")
+//update user
+router.put("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10)
+                req.body.password = await bcrypt.hash(req.body.password, salt)
+            }catch(err){
+                return res.status(500).json(err)
+            }
+        }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id,{
+                $set: req.body,
+            })
+            res.status(200).json("information updated!")
+        }catch(err){
+            return res.status(500).json(err)
+        }
+    }else{
+        return res.status(403).json("You can only update your account!")
+    }
 })
-
+//delete user
+router.delete("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        try{
+            await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("account deleted!")
+        }catch(err){
+            return res.status(500).json(err)
+        }
+    }else{
+        return res.status(403).json("You can only delete your account!")
+    }
+})
+//get user
+router.get("/:id", async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+        const {password,updatedAt, ...other} = user._doc
+        res.status(200).json(other)
+    }catch(err){
+        return res.status(500).json(err)
+    }
+})
 
 
 module.exports = router
