305b9a82ad4e4790c03d75651dac9878658fffe3
Crisila <crisilaaban@gmail.com>
2023-05-26 18:15:20 -0700

    style & register page

---
+++ thrive-chat/src/pages/login/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div className='form-container'>
+      <div className="form-wrapper">
+        <span className='thrivechat'>ThriveChat</span>
+        <span className="title">Register</span>
+        <form>
+          <input
+            className='input-name'
+            type="text"
+            placeholder='name' />
+          <input
+            className='input-email'
+            type="email"
+            placeholder='email' />
+          <input
+            className='input-password'
+            type="password"
+            placeholder='******' />
+          <input
+            className='input-file'
+            type="file" />
+          <button className='signup-button'>Sign up</button>
+        </form>
+
+        <p className='signup-login'>I have an account. Login</p>
+
+      </div>
+    </div>
+  )
+}
+
+
+export default login
