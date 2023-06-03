c40320fba84552caaa82941e96deba93141d082f
Ashley Kubizna <113566558+kubizna21@users.noreply.github.com>
2023-06-02 17:10:17 -0700

    changed some css

--- thrive-chat/src/components/Search.jsx
+++ thrive-chat/src/components/Search.jsx
@@ -1,32 +1,33 @@
 import React, { useState } from 'react'
 
 const Search = () => {
-  // const [username, setUsername] = useState("")
-  // const [user, setUser] = useState(null)
-  // const [err, setErr] = useState(false)
+ const [username, setUsername] = useState("")
+ const [user, setUser] = useState(null)
+ const [err, setErr] = useState(false)
 
-  // const handleSearch = () => {
+ const handleSearch = () => {
 
-  // }
+ }
 
-  // const handleKey = e =>{
-  //   e.code === "Enter" && handleSearch()
-  // }
+ const handleKey = e =>{
+   e.code === "Enter" && handleSearch()
+ }
 
   return (
     <div className='search'>
       <div className="search-form">
-        {/* <input type="text" placeholder='Find a User' onKeyDown={handleKey} onChange={e=> setUsername(e.target.value)}/> */}
+        { <input type="text" placeholder='Find a User' onKeyDown={handleKey} onChange={e=> setUsername(e.target.value)}/>}
       </div>
-      {/* <div className="user-chats">
+      { <div className="user-chats">
         <img src="https://images.pexels.com/photos/16796419/pexels-photo-16796419/free-photo-of-woman-posing-near-vegetables-on-bazaar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" width={30} height={30} alt="user-chat-img"></img>
         <div className="user-chats-info">
-          <span>User-reciever-1</span>
+        <span>User-reciver-1</span>
         </div>
 
-      </div> */}
+      </div> }
     </div>
   )
 }
 
 export default Search
+
